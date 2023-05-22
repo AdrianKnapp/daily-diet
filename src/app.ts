00181804af02fdf016063c9c3ba35a1a5fede547
AdrianKnapp <adrknapp@gmail.com>
2023-05-22 08:52:37 -0300

    chore: added eslint

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-export const app = fastify();
+export const app = fastify()
 
 // app.addHook("onRequest", (request, reply, done) => {
 //   console.log(`🚀 request ~ [${request.method}] ${request.url}`)
 // });
 
-app.get("/", async (request, reply) => {
-  reply.send({ hello: "world" })
-});
+app.get('/', async (request, reply) => {
+  reply.send({ hello: 'world' })
+})
