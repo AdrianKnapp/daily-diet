7668d996c55c40c73d3dc5c4db3dfc21c234010a
AdrianKnapp <adrknapp@gmail.com>
2023-05-24 08:17:43 -0300

    adjusted migrations

--- db/migrations/20230524110405_meals.ts
+++ db/migrations/20230524110405_meals.ts
@@ -1,18 +1,18 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  knex.schema.createTable('meals', (table) => {
+  return knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
     table.string('name').notNullable()
     table.string('description').notNullable()
-    table.dateTime('date').notNullable()
+    table.dateTime('date').notNullable().defaultTo(knex.fn.now())
     table.boolean('is_in_the_diet').notNullable()
     table.uuid('user_id').notNullable()
     table.foreign('user_id').references('id').inTable('users')
-    table.dateTime('created_at').notNullable()
+    table.dateTime('created_at').notNullable().defaultTo(knex.fn.now())
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  knex.schema.dropTable('meals')
+  return knex.schema.dropTable('meals')
 }
--- db/migrations/20230524111005_users.ts
+++ db/migrations/20230524111005_users.ts
@@ -1,13 +1,15 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
     table.string('email').notNullable()
     table.string('password').notNullable()
     table.string('name').notNullable()
-    table.dateTime('created_at').notNullable()
+    table.dateTime('created_at').notNullable().defaultTo(knex.fn.now())
   })
 }
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users')
+}
