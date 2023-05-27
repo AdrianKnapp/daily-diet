c6c87dbac31c4a98bd62c3642d41ed580c3aa649
AdrianKnapp <adrknapp@gmail.com>
2023-05-27 09:57:53 -0300

    feat: added vitest and supertest

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,14 @@
 import { knex as knexSetup, type Knex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    database: './db/app.db',
-    filename: './db/app.db',
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
