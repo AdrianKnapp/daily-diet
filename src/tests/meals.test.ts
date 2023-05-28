2ba597254b7f4aad4ca4b066290198f56ac6f283
AdrianKnapp <adrknapp@gmail.com>
2023-05-28 20:43:13 -0300

    test: create meal

---
+++ src/tests/meals.test.ts
@@ -0,0 +1,50 @@
+import { afterAll, beforeAll, expect, describe, it, beforeEach } from 'vitest'
+import request from 'supertest'
+import { execSync } from 'node:child_process'
+import { app } from '../app'
+import { joeDoe } from './mocks/users'
+import { lunchMock } from './mocks/meals'
+
+const login = async () => {
+  const loginResponse = await request(app.server)
+    .post('/users')
+    .send(joeDoe)
+    .expect(201)
+
+  const loginCookies = loginResponse.get('Set-Cookie')
+
+  return loginCookies
+}
+
+const createMeal = async (cookies) => {
+  const response = await request(app.server)
+    .post('/meals')
+    .send(lunchMock)
+    .set('Cookie', cookies)
+    .expect(201)
+
+  return response
+}
+
+describe('meals routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new meal', async () => {
+    const cookies = await login()
+
+    const response = await createMeal(cookies)
+
+    expect(response.statusCode).toBe(201)
+  })
+})
---
+++ src/tests/mocks/meals.ts
@@ -0,0 +1,6 @@
+export const lunchMock = {
+  name: 'Pão',
+  description: 'Café da manhã',
+  date: 'Tue May 23 2023 09:08:17 GMT-0300',
+  isInTheDiet: true,
+}
